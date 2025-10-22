fd3215b5e47fa50ae74115f1e66c7292ec0e65fc
KaicyB <Barka901@school.lu>
2025-10-22 11:12:44 +0200

    Added a navbar and pages
    added a navbar and a home page

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/home")
+def homepage():
+
+    return render_template("home.html")
+
 
 @app.route("/about")
 def about():
